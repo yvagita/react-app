bd9bba8ccd1cc91ea382eed7bcbd053648049c48
yvagitau <gitauenjeri@gmail.com>
2023-08-21 14:17:37 +0300

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -79,8 +79,15 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         quantity: 1
       });
     }
+      //adding quantity totals
+      let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
    
-    console.log(cart);
   });
 });
 
