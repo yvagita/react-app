8e0b5a6741f473df4f56978552c2c36adb46d0df
yvagitau <gitauenjeri@gmail.com>
2023-08-22 13:51:51 +0300

    Fixed radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -53,7 +53,7 @@ cart.forEach((cartItem) => {
         <div class="delivery-option">
           <input type="radio" checked
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${mathchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Tuesday, June 21
@@ -66,7 +66,7 @@ cart.forEach((cartItem) => {
         <div class="delivery-option">
           <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${mathchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Wednesday, June 15
@@ -79,7 +79,7 @@ cart.forEach((cartItem) => {
         <div class="delivery-option">
           <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-1">
+            name="delivery-option-${mathchingProduct.id}">
           <div>
             <div class="delivery-option-date">
               Monday, June 13
